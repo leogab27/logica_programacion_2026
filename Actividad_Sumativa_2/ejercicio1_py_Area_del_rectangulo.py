baf336ca1661085523befae4c5d6250b4f259ebc
leogab27 <leo2007gab@gmail.com>
2026-08-17 14:53:48 -0600

    Delete Actividad_Sumativa_2 directory

--- Actividad_Sumativa_2/ejercicio1_py_Area_del_rectangulo.py
+++
@@ -1,5 +0,0 @@
-#Programa para calcular el área de un rectángulo
-base = float (input ("Ingrese la base: "))
-altura = float (input ("Ingrese la altura: "))
-area = base * altura
-print ("El área del rectángulo es:", area)
--- Actividad_Sumativa_2/ejercicio3_py_IMC.py
+++
@@ -1,5 +0,0 @@
-# Programa para calcular el IMC
-peso = float(input("Peso (kg): "))
-altura = float(input("Altura (m): "))
-imc = peso / (altura ** 2)
-print("Su IMC es:", imc)
